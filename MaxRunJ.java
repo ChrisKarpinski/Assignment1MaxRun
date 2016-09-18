ac20af1884b7a32eb02945d17a487a60fbdb745c
ChrisKarpinski <christopher.karpinski@ocsbstudent.ca>
2016-09-18 13:21:02 -0400

    Update MaxRunJ.java

--- MaxRunJ.java
+++ MaxRunJ.java
@@ -11,65 +11,75 @@ import java.util.Scanner;
 
 public class MaxRun {
 
-    public static void main(String[] args) {
-
-        int charCount = 1;
-        int maxCount = 1;
+	public static void main(String[] args) {
+		
+		// Scanner class instantiated to be able to read user input
+		Scanner readInput = new Scanner(System.in);
+		
         String enteredString;
         String wantContinue = "Y";
-
-        // Scanner class instantiated to be able to read user input
-        Scanner readInput = new Scanner(System.in);
-
+        
         while (wantContinue.equals("Y")) {
+        	
+        	System.out.println("Enter the string");
+        	enteredString = readInput.next();
+        	
+        	System.out.println("The max running length in the string is: " + 
+        	FindMaxRun(enteredString));
+            System.out.println("Would you like to continue? (enter Y or N)");
+            
+            wantContinue = readInput.next();
 
-            System.out.println("Enter the string");
-            enteredString = readInput.next();
+            while (!(wantContinue.equals("Y") || wantContinue.equals("N"))) {
+                // makes sure user enters valid option for continuation
 
-            for (int iterator = 0; iterator < enteredString.length()-1; iterator++) {
-                // this for loop loops through all characters in the string
+                System.out.println("Invalid option entered");
+                System.out.println("Would you like to continue? "
+                		+ "(enter Y or N)");
+                wantContinue = readInput.next();
+            }
+        }
+        
+        if (wantContinue.equals("N")) {
+            // finishes the program if continuation is not wanted
+            System.out.println("");
+            System.out.println("Done.");
+        }
 
-                if (enteredString.charAt(iterator) == enteredString.charAt(iterator + 1)) {
-                    // this if statement checks if the next character is the same as the current
+	}
+	
+	public static int FindMaxRun (String inputString) {
+		// this function finds the max run of a string
+		
+		int charCount = 1;
+        int maxCount = 1;
+		
+		for (int iterator = 0; iterator < inputString.length()-1; 
+				iterator++) {
+            // this for loop loops through all characters in the string
 
-                    charCount++;
+            if (inputString.charAt(iterator) == inputString.charAt
+            		(iterator + 1)) {
+                // this if statement checks if the next character is 
+            	//     the same as the current
 
-                    if (charCount > maxCount) {
-                        // setting the max count to the running count if the running count
-                        //    is greater
-                        maxCount = charCount;
-                    }
-                }
-                  else {
-                    // reset the running count if the consecutive run of a character ends
-                    charCount = 1;
+                charCount++;
+
+                if (charCount > maxCount) {
+                    // setting the max count to the running count if the 
+                    //    running count is greater
+                    maxCount = charCount;
                 }
             }
-
-         System.out.println("The max running length in the string is: " + maxCount);
-         System.out.println("Would you like to continue? (enter Y or N)");
-
-         wantContinue = readInput.next();
-
-         while (!(wantContinue.equals("Y") || wantContinue.equals("N"))) {
-             // makes sure user enters valid option for continuation
-
-             System.out.println("Invalid option entered");
-             System.out.println("Would you like to continue? (enter Y or N)");
-             wantContinue = readInput.next();
-         }
-        // reset the max count back to start at 1 for new input
-         maxCount = 1;
-
-         if (wantContinue.equals("N")) {
-             // finishes the program if continuation is not wanted
-             System.out.println("");
-             System.out.println("Done.");
-         }
-
-
-         }
-
-    }
+              else {
+                // reset the running count if the consecutive run of 
+            	//    a character ends
+                charCount = 1;
+            }
+        }
+		
+		return maxCount;
+		
+	}
 
 }
